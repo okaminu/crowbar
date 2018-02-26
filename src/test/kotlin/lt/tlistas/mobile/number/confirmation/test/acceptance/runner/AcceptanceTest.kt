a42fcf501ba214c49e882c4a504699d1f97e512d
Kastytis Chainauskas <kastytis.ch@gmail.com>
2018-02-26 15:05:13 +0200

    TLIST-466: removed cucumber.gradle file

--- src/test/kotlin/lt/tlistas/mobile/number/confirmation/test/acceptance/runner/AcceptanceTest.kt
+++ src/test/kotlin/lt/tlistas/mobile/number/confirmation/test/acceptance/runner/AcceptanceTest.kt
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith
 
 @RunWith(Cucumber::class)
 @CucumberOptions(
+        format = arrayOf("progress"),
         features = arrayOf("src/test/resources/feature"),
         glue = arrayOf("lt.tlistas.mobile.number.confirmation.test.acceptance.step"))
 class AcceptanceTest
