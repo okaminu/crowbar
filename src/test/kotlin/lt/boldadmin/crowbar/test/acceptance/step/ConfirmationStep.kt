677bf0886028c8da4d861a2c2d44452c5eb0cbf3
vsh1ft <kastytis.ch@gmail.com>
2019-07-25 12:49:04 +0300

    BA-901: updated cucumber version

--- src/test/kotlin/lt/boldadmin/crowbar/test/acceptance/step/ConfirmationStep.kt
+++ src/test/kotlin/lt/boldadmin/crowbar/test/acceptance/step/ConfirmationStep.kt
@@ -1,9 +1,9 @@
 package lt.boldadmin.crowbar.test.acceptance.step
 
-import cucumber.api.java.Before
-import cucumber.api.java.en.Given
-import cucumber.api.java.en.Then
-import cucumber.api.java.en.When
+import io.cucumber.java.Before
+import io.cucumber.java.en.Given
+import io.cucumber.java.en.Then
+import io.cucumber.java.en.When
 import io.mockk.*
 import io.mockk.impl.annotations.MockK
 import lt.boldadmin.crowbar.IdentityConfirmation
